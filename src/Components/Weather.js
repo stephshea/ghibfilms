fde4f58050f27311252e13b3156d3fa3693de9f5
Stephanie <stephcshea@gmail.com>
2018-09-19 20:33:39 -0400

    updating

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Weather = () => {
+class Weather extends Component {
+
+  // state = {
+  //   weather: []
+  // }
+
+  componentDidMount() {
+    axios.get('http://api.openweathermap.org/data/2.5/weather?&units=metric&APPID= ae36112ec88d99f153f8a75484b09f50')
+    .then(res => {
+      // this.setState({
+      //   weather: res.data
+      // });
+      console.log(res);
+    })
+    // .catch(error => {
+    //   console.log('Error', error);
+    // });
+  };
+
+  render() {
   return (
-    <div className="containter">
+    <div className="container">
       <h4 className="center">Weather</h4>
     </div>
   )
 }
-
+}
 export default Weather
